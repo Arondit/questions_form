cf2763eda41a1623925c01085373057ff5ad3276
Arondit <topoohto@gmail.com>
2022-04-22 20:38:05 +0300

    add node creation

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 
-from app.config import AppConfig
+from app.config import app_config
+from app.neo4j_connection import create_node
 
 
 app = FastAPI()
 
 
+urls_base = {
+    'question_form': '/question-form/',
+}
+
+
 @app.get('/')
-def main_apge():
+def main_page():
     """Главная страница"""
     return {'status': 'В разработке'}
 
@@ -15,10 +21,11 @@ def main_apge():
 @app.post('/question-form/create')
 def create_empty_question_form():
     """Создает пустой опросник"""
+    create_node('QuestionForm', author_name='Vlad')
     return {'some_field': 'success'}
 
 
 @app.get('/test/env')
 def is_heroku():
     """Корректно ли работает env конфиг в связке с Heroku"""
-    return {'is_heroku': AppConfig.IS_HEROKU}
+    return {'is_heroku': app_config.IS_HEROKU}
--- app/neo4j_connection.py
+++ app/neo4j_connection.py
@@ -1,25 +1,25 @@
-from neo4j import GraphDatabase
+from datetime import datetime
+from neo4j import GraphDatabase, Neo4jDriver
 
 from app.config import neo4j_config
 
 
-def get_driver():
+def get_driver() -> Neo4jDriver:
     """Возвращает драйвер подключения к Neo4j согласно настройкам в .env"""
     return GraphDatabase.driver(neo4j_config.uri, auth=(neo4j_config.username, neo4j_config.password))
 
 
-def do_cypher_tx(tx, cypher):
+def do_cypher_tx(tx, class_name, **attributes):
     """Вспомогательная функция для запуска Cypher-запроса"""
-    tx.run(cypher)
+    attributes_string = '{' + ', '.join(f'{key}: "{value}"' for key, value in attributes.items()) + '}'
+    return tx.run(f'create (node:{class_name} {attributes_string}) return node;')
 
 
 def create_node(class_name, **attributes):
     """Создает в Neo4j вершину с указанным классом и аттрибутами"""
     driver = get_driver()
-    attributes_string = '{' + ', '.join(f'{key}: "{value}"' for key, value in attributes.items()) + '}'
+
+    attributes['datetime_created'] = datetime.now()
 
     with driver.session() as session:
-        values = session.write_transaction(do_cypher_tx, f'create (:{class_name} {attributes_string})')
-    driver.close()
-
-    return values
+        session.write_transaction(do_cypher_tx, class_name=class_name, **attributes)
