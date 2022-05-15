8a63e54f8a4ccd9ab196602208006daa8b6cf748
Arondit <topoohto@gmail.com>
2022-05-15 21:17:23 +0300

    Add tags and endpoints for answering

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
 from secrets import compare_digest
+import uuid
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+from app.constants import (
+    QUESTION_FORM_URL,
+    QUESTIONS_URL,
+    AUTH_TAGS,
+    QUESTION_FORM_TAGS,
+    QUESTIONS_TAGS,
+    ANSWERING_TAGS,
+)
 from app.neo4j_tools import create_node, edit_node
-from app.neo4j_tools.create import create_question_for_form, create_token_authorization
-from app.neo4j_tools.match_return import get_forms_by_username, get_node_by_id, get_questions_by_form_id, get_user_by_token, get_user_by_username
+from app.neo4j_tools.auth import (
+    create_token_authorization,
+    get_user_by_token, 
+    get_user_by_username,
+)
+from app.neo4j_tools.create import answer_for_question, create_question_for_form
+from app.neo4j_tools.match_return import (
+    get_form_by_code,
+    get_forms_by_username, 
+    get_node_by_id,
+    get_question_answers, 
+    get_questions_by_form_id,
+)
 
 import app.request_models as request_models
 import app.response_models as response_models
@@ -51,7 +70,7 @@ def main_page():
     return {'status': 'В разработке'}
 
 
-@app.post('/register/', name='Регистрация')
+@app.post('/register/', name='Регистрация', tags=AUTH_TAGS)
 def register(body: UserWithPassword):
     """Создание пользователя с указанными логином и паролем."""
     create_node('User', **body.dict())
@@ -60,7 +79,7 @@ def register(body: UserWithPassword):
     return {'access_token': token, 'token_type': 'bearer'}
 
 
-@app.post('/login/', name='Авторизация', response_model=Token)
+@app.post('/login/', name='Авторизация', response_model=Token, tags=AUTH_TAGS)
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     """Создает в базе данных токен для пользователя и возвращает в ответе."""
     user = get_user_by_username(form_data.username)
@@ -78,10 +97,12 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     return {'access_token': token, 'token_type': 'bearer'}
 
 
-QUESTION_FORM_URL = '/question-form'
-
-
-@app.get(QUESTION_FORM_URL + '/all/', response_model=response_models.AccountForms, name='Опросники пользователя')
+@app.get(
+    QUESTION_FORM_URL + '/all/', 
+    response_model=response_models.AccountForms, 
+    name='Опросники пользователя',
+    tags=QUESTION_FORM_TAGS,
+)
 def get_user_question_forms(current_user: User = Depends(get_current_user)) -> response_models.AccountForms:
     """Выводит все опросники, которые создал пользователь"""
     question_forms = get_forms_by_username(current_user.username)
@@ -89,7 +110,12 @@ def get_user_question_forms(current_user: User = Depends(get_current_user)) -> r
     return response_models.AccountForms(forms=[{**q['node'], 'id': q['node'].id} for q in question_forms])
 
 
-@app.get(QUESTION_FORM_URL + '/{form_id}/', response_model=response_models.QuestionForm, name='Опросник')
+@app.get(
+    QUESTION_FORM_URL + '/{form_id}/', 
+    response_model=response_models.QuestionForm, 
+    name='Опросник', 
+    tags=QUESTION_FORM_TAGS,
+)
 def get_question_form(form_id: int, current_user: User = Depends(get_current_user)) -> response_models.QuestionForm:
     """Выводит подробную информацию об опроснике по id"""
     question_form = get_node_by_id('QuestionForm', form_id)
@@ -98,34 +124,105 @@ def get_question_form(form_id: int, current_user: User = Depends(get_current_use
     question_form = question_form['node']
     questions = get_questions_by_form_id(form_id)
 
-    questions = [{**q['node'], 'id': q['node'].id} for q in questions]
+    questions = [{**question['node'], 'id': question['node'].id} for question in questions]
 
     return {**question_form, 'id': question_form.id, 'questions': questions}
 
 
-@app.post(f'{QUESTION_FORM_URL}/create/', name='Новый опросник')
+@app.post(f'{QUESTION_FORM_URL}/create/', name='Новый опросник', tags=QUESTION_FORM_TAGS)
 def create_empty_question_form(current_user: User = Depends(get_current_user)):
     """Создает пустой опросник"""
-    create_node('QuestionForm', username=current_user.username, name='Пустая форма', theme='Нет темы')
+    form_code = str(uuid.uuid4())[:8]
+    create_node('QuestionForm', username=current_user.username, name='Пустая форма', theme='Нет темы', code=form_code)
     return Response(status_code=status.HTTP_201_CREATED)
 
 
-@app.patch(QUESTION_FORM_URL + '/{form_id}/edit/', name='Редактировать опросник')
-def edit_question_form(form_id: int, body: request_models.QuestionForm, current_user: User = Depends(get_current_user)):
+@app.patch(QUESTION_FORM_URL + '/{form_id}/edit/', name='Редактировать опросник', tags=QUESTION_FORM_TAGS)
+def edit_question_form(
+    form_id: int, 
+    body: request_models.QuestionForm, 
+    current_user: User = Depends(get_current_user),
+):
     """Редактирование опросника"""
     edit_node('QuestionForm', node_id=form_id, **body.dict())
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.post(QUESTION_FORM_URL + '/{form_id}/questions/create/', name='Новый вопрос')
-def create_empty_question(form_id: int, body: request_models.Question, current_user: User = Depends(get_current_user)):
+@app.post(QUESTION_FORM_URL + '/{form_id}/create-question/', name='Новый вопрос', tags=QUESTIONS_TAGS)
+def create_empty_question(
+    form_id: int, 
+    body: request_models.Question, 
+    current_user: User = Depends(get_current_user),
+):
     """Создает пустой вопрос"""
     create_question_for_form(form_id=form_id, **body.dict())
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.patch(QUESTION_FORM_URL + '/questions/{question_id}/edit/', name='Редактировать вопрос')
-def edit_question(question_id: int, body: request_models.Question, current_user: User = Depends(get_current_user)):
+@app.patch(QUESTIONS_URL + '/{question_id}/edit/', name='Редактировать вопрос', tags=QUESTIONS_TAGS)
+def edit_question(
+    question_id: int, 
+    body: request_models.Question, 
+    current_user: User = Depends(get_current_user),
+):
     """Редактирование вопроса"""
     edit_node('Question', node_id=question_id, **body.dict())
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.get(
+    QUESTIONS_URL + '/{question_id}/answers/', 
+    name='Ответы на вопрос', 
+    response_model=response_models.QuestionResult,
+    tags=QUESTIONS_TAGS,
+)
+def question_answers(question_id: int, current_user: User = Depends(get_current_user)):
+    """Ответы на вопрос"""
+    return get_question_answers(question_id)
+
+
+@app.post(
+    QUESTIONS_URL + '/{question_id}/answers/create', 
+    name='Ответить на вопрос', 
+    response_model=response_models.Answer,
+    tags=ANSWERING_TAGS,
+)
+def answer_question(question_id: int, body: request_models.Answer):
+    """Ответить на вопрос"""
+    question = get_node_by_id('Question', node_id=question_id)
+    if not question:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    question = question['node']
+    correct_answer = question.get('correct_answer', None)
+    
+    if correct_answer is None:
+        is_correct = None
+    elif correct_answer == body.text:
+        is_correct = True
+    else:
+        is_correct = False
+
+    result = {'text': body.text, 'is_correct': is_correct, 'correct_answer': correct_answer}
+    answer_for_question(question_id, **result)
+    
+    return result
+
+
+@app.get(
+    '/by-code/{form_code}/', 
+    name='Пройти опрос', 
+    response_model=response_models.QuestionForm, 
+    tags=ANSWERING_TAGS,
+)
+def get_answer_form_for_answering(form_code: str):
+    """Получает форму для прохождения опросника по коду"""
+    question_form = get_form_by_code(form_code)
+    if not question_form:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    question_form = question_form['node']
+    questions = get_questions_by_form_id(question_form.id)
+
+    questions = [{**question['node'], 'id': question['node'].id} for question in questions]
+
+    return {**question_form, 'id': question_form.id, 'questions': questions}
