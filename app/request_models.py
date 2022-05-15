b068a89e39e257a0986f32cf6bba80fd01484a02
Arondit <topoohto@gmail.com>
2022-05-15 21:15:35 +0300

    Add models for answering

--- app/request_models.py
+++ app/request_models.py
@@ -8,10 +8,14 @@ class Question(BaseModel):
     text: str
     question_type: QuestionType = QuestionType.text.name
     variants: Optional[List[str]] = None
+    correct_answer: Optional[str] = None
 
 
 class QuestionForm(BaseModel):
     name: Optional[str] = None
-    username: Optional[str] = None
     theme: Optional[str] = None
     is_quiz: Optional[bool] = None
+
+
+class Answer(BaseModel):
+    text: str
--- app/response_models.py
+++ app/response_models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List, Optional, Union
+from typing import List, Optional, Dict
 from pydantic import BaseModel
 
 from app.constants import QuestionType
@@ -11,6 +11,7 @@ class Question(BaseModel):
     question_type: QuestionType
     datetime_created: datetime
     variants: Optional[List[str]] = None
+    correct_answer: Optional[str] = None
 
     def __init__(__pydantic_self__, **data) -> None:
         variants: str = data['variants']
@@ -23,9 +24,23 @@ class QuestionForm(BaseModel):
     id: int
     name: str
     theme: str
+    code: str
     datetime_created: datetime
+    is_quiz: Optional[bool] = None
     questions: List[Question] = []
 
 
 class AccountForms(BaseModel):
     forms: List[QuestionForm] = []
+
+
+class Answer(BaseModel):
+    text: str
+    is_correct: Optional[bool] = None
+    correct_answer: Optional[str] = None
+
+
+class QuestionResult(BaseModel):
+    answers: Dict[str, int]
+    all_answers_count: int
+    correct_answers_count: int
